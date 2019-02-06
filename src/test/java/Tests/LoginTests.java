38f59a8d7927c6c7876bda6b77b7530114dc419f
Huseyin <hbalcayev@gmail.com>
2019-02-05 23:31:18 -0600

    First commit, Login Test1

---
+++ src/test/java/Tests/LoginTests.java
@@ -0,0 +1,49 @@
+package Tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+
+    @Test
+    public void loginTest1(){
+driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+
+        WebElement username=driver.findElement(By.id("ctl00_MainContent_username"));
+        WebElement password=driver.findElement(By.id("ctl00_MainContent_password"));
+        WebElement loginBtn=driver.findElement(By.id("ctl00_MainContent_login_button"));
+
+        username.sendKeys("Tester");
+        password.sendKeys("test");
+        loginBtn.click();
+
+        String newTitle=driver.getTitle();
+        if(newTitle.equals("Web Orders")){
+            System.out.println("Web title is verified");
+        }else{
+            System.out.println("Not verified");
+        }
+    }
+
+
+}
